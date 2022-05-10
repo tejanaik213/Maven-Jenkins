c36cd6831a2788e75b54081de5c8c0f0c8b86602
tejanaik213 <104199953+tejanaik213@users.noreply.github.com>
2022-05-10 17:16:17 +0530

    Update ReadingDataFromCommandLineTest.java

--- src/test/java/MavenExample/ReadingDataFromCommandLineTest.java
+++ src/test/java/MavenExample/ReadingDataFromCommandLineTest.java
@@ -8,7 +8,7 @@ public class ReadingDataFromCommandLineTest {
 	String	URL=System.getProperty("url");
 	String UN = System.getProperty("username");
 	String PWD = System.getProperty("password");
-		System.out.println("is the");
+		System.out.println("Changes Done");
 	System.out.println(URL);
 	System.out.println(UN);
 	System.out.println(PWD);
